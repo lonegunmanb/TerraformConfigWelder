6be07cbb76fc8a5e31173b0d7344777890cf6e1c
hezijie <lonegunmanb@hotmail.com>
2024-08-28 15:52:15 +0800

    azurerm_cosmosdb_sql_container

--- azurerm/v3_v4/azurerm_cosmosdb_sql_container.mptf.hcl
+++ azurerm/v3_v4/azurerm_cosmosdb_sql_container.mptf.hcl
@@ -22,3 +22,12 @@ transform "remove_block_element" cosmosdb_sql_with_partition_key_path {
     transform.update_in_place.cosmosdb_sql_with_partition_key_path,
   ]
 }
+
+transform regex_replace_expression cosmosdb_sql_with_partition_key_path {
+  for_each    = var.azurerm_cosmosdb_account_toggle ? ["cosmosdb_sql_with_partition_key_path"] : []
+  regex       = "(^|[^d]$|[^a]d$|[^t]da$|[^a]dat$|[^.]data$)azurerm_cosmosdb_sql_container\\.(\\s*\\r?\\n\\s*)?(\\w+)(\\[\\s*[^]]+\\s*\\])?(\\.)(\\s*\\r?\n\\s*)?partition_key_path([^s]|$)"
+  replacement = "azurerm_cosmosdb_sql_container.$${1}$${2}$${3}$${4}$${5}partition_key_paths[0]"
+  depends_on = [
+    transform.rename_block_element.simply_renamed,
+  ]
+}
--- azurerm/v3_v4/test-fitness/main.tf
+++ azurerm/v3_v4/test-fitness/main.tf
@@ -282,33 +282,6 @@ resource "azurerm_key_vault_managed_hardware_security_module_role_assignment" "t
   vault_base_url     = var.key_vault_managed_hardware_security_module_role_assignment_vault_base_url
 }
 
-resource "azurerm_cosmosdb_sql_container" "example" {
-  account_name          = data.azurerm_cosmosdb_account.example.name
-  database_name         = azurerm_cosmosdb_sql_database.example.name
-  name                  = "example-container"
-  resource_group_name   = data.azurerm_cosmosdb_account.example.resource_group_name
-  partition_key_path    = "/definition/id"
-  partition_key_version = 1
-  throughput            = 400
-
-  indexing_policy {
-    indexing_mode = "consistent"
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-    included_path {
-      path = "/*"
-    }
-    included_path {
-      path = "/included/?"
-    }
-  }
-  unique_key {
-    paths = ["/definition/idlong", "/definition/idshort"]
-  }
-}
-
 resource "azurerm_databricks_workspace" "example" {
   location                              = azurerm_resource_group.example.location
   name                                  = "databricks-test"
