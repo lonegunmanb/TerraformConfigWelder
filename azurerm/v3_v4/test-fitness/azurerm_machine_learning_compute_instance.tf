1be039a2801db237cfd433a9c52d96974637fe28
hezijie <lonegunmanb@hotmail.com>
2024-08-29 16:06:37 +0800

    azurerm_machine_learning_compute_instance

---
+++ azurerm/v3_v4/test-fitness/azurerm_machine_learning_compute_instance.tf
@@ -0,0 +1,16 @@
+resource "azurerm_machine_learning_compute_instance" "example" {
+  machine_learning_workspace_id = azurerm_machine_learning_workspace.example.id
+  name                          = "example"
+  virtual_machine_size          = "STANDARD_DS2_V2"
+  authorization_type            = "personal"
+  description                   = "foo"
+  location                      = azurerm_machine_learning_workspace.example.location
+  subnet_resource_id            = azurerm_subnet.example.id
+  tags = {
+    foo = "bar"
+  }
+
+  ssh {
+    public_key = var.ssh_key
+  }
+}
