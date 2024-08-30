09ebfb82db9e8272d59b559c4fe7f90c2fed6871
lonegunmanb <lonegunmanb@hotmail.com>
2024-08-31 06:20:20 +0800

    azurerm_resource_group_policy_remediation

---
+++ azurerm/v3_v4/test-fitness/azurerm_resource_group_policy_remediation.tf
@@ -0,0 +1,11 @@
+resource "azurerm_resource_group_policy_remediation" "example" {
+  name                 = "example-policy-remediation"
+  policy_assignment_id = azurerm_resource_group_policy_assignment.example.id
+  resource_group_id    = azurerm_resource_group.example.id
+  location_filters     = ["West Europe"]
+  policy_definition_id = var.azurerm_resource_group_policy_remediation_policy_definition_id
+}
+
+locals {
+  azurerm_resource_group_policy_remediation_policy_definition_id = azurerm_resource_group_policy_remediation.example.policy_definition_id
+}
