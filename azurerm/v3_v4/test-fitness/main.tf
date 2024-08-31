89d4232d9fb1139016add75033566310e67aff1b
lonegunmanb <lonegunmanb@hotmail.com>
2024-08-31 14:35:01 +0800

    azurerm_sentinel_alert_rule_scheduled

--- azurerm/v3_v4/test-fitness/main.tf
+++ azurerm/v3_v4/test-fitness/main.tf
@@ -102,33 +102,6 @@ resource "azurerm_monitor_action_group" "example" {
   }
 }
 
-resource "azurerm_sentinel_alert_rule_scheduled" "example" {
-  display_name               = "example"
-  log_analytics_workspace_id = azurerm_sentinel_log_analytics_workspace_onboarding.example.workspace_id
-  name                       = "example"
-  query                      = <<QUERY
-AzureActivity |
-  where OperationName == "Create or Update Virtual Machine" or OperationName =="Create Deployment" |
-  where ActivityStatus == "Succeeded" |
-  make-series dcount(ResourceId) default=0 on EventSubmissionTimestamp in range(ago(7d), now(), 1d) by Caller
-QUERY
-  severity                   = "High"
-
-  incident_configuration {
-    create_incident         = var.azurerm_sentinel_alert_rule_scheduled_incident_configuration_create_incident
-    group_by_alert_details  = var.azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_alert_details
-    group_by_custom_details = var.azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_custom_details
-    group_by_entities       = var.azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_entities
-  }
-}
-
-locals {
-  azurerm_sentinel_alert_rule_scheduled_incident_configuration_create_incident         = azurerm_sentinel_alert_rule_scheduled.example.incident_configuration[0].create_incident
-  azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_alert_details  = azurerm_sentinel_alert_rule_scheduled.example.incident_configuration[0].group_by_alert_details
-  azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_custom_details = azurerm_sentinel_alert_rule_scheduled.example.incident_configuration[0].group_by_custom_details
-  azurerm_sentinel_alert_rule_scheduled_incident_configuration_group_by_entities       = azurerm_sentinel_alert_rule_scheduled.example.incident_configuration[0].group_by_entities
-}
-
 resource "azurerm_sentinel_log_analytics_workspace_onboarding" "singleton" {
   customer_managed_key_enabled = false
   resource_group_name          = var.azurerm_sentinel_log_analytics_workspace_onboarding_resource_group_name
